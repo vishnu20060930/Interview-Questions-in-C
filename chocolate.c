b4f1173be490f43a0ab862a2fe9741faa69290a1
vishnu20060930 <vishnuvardhanmudhiraj67@gmail.com>
2026-02-03 15:42:23 +0530

    Add files via upload

---
+++ chocolate.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void main()
+{
+    int big;
+    int small;
+    int goal;
+    printf("enter the amount of big bars\n");
+    scanf("%d",&big);
+    printf("enter the amount of small bars\n");
+    scanf("%d",&small);
+    printf("enter the amount of chocolate to be packed\n");
+    scanf("%d",&goal);
+    int req=goal/5;
+    if(req<=big)
+    {
+        int rem=goal-(req*5);
+        if(rem<=small)
+        {
+            printf("%d",rem);
+        }
+        else
+        {
+            printf("-1");
+        }
+    }
+    else
+    {
+        int rem1=goal-(big*5);
+        if(rem1<=small)
+        {
+            printf("%d",rem1);
+        }
+        else
+        {
+            printf("-1");
+        }
+    }
+}
