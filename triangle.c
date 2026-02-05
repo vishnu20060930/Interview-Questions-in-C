6198d5f86bf6d75408942c1fb088168c0380186f
vishnu20060930 <vishnuvardhanmudhiraj67@gmail.com>
2026-02-05 06:43:37 +0530

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+int clmn;
+int rows;
+printf("enter the amount of rows:\n");
+scanf("%d",&rows);
+printf("enter the amount of columns:\n");
+scanf("%d",&clmn);
+for(int i=1;i<=rows;i++)
+{
+for (int j=1;j<=clmn;j++)
+{
+if(j==1||i==rows||j==1||i>j||i==j)
+
+{
+printf("* ");
+}
+else
+{
+printf("  ");
+}
+}
+printf("\n");
+}
+}
